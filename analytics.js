a0a928ab13845f46b3bcd3b8e8a2311a0a3281a6
Denis Golubovskiy <bukashk0zzz@me.com>
2021-06-22 07:35:16 +0300

    Shopify update fix

--- analytics.js
+++ analytics.js
@@ -1,46 +1,50 @@
 window.dataLayer = window.dataLayer || [];
 
-var patchedTrack =  function(type, data, eventId) {    
-    if (!eventId) {
-        eventId = Math.random().toString(36).substr(2, 9) + '_' + Math.random().toString(36).substr(2, 9) +'_' + Math.random().toString(36).substr(2, 9);
-    }
-    
-    if (type === 'Viewed Product' || type === 'Added Product') {
-        var dataLayerData = data;  
-
-        dataLayerData.content_type = data.productId ? 'product_group' : 'product';
-        dataLayerData.event_id = eventId;
-        dataLayerData.event = type;
-
-        dataLayer.push(dataLayerData);
-    }
-
-    window.ShopifyAnalytics.lib.track2(type, data, eventId);
-};
-
-function patchTrack() {
-    if (window.ShopifyAnalytics &&
-        window.ShopifyAnalytics.lib &&
-        window.ShopifyAnalytics.meta &&
-        window.ShopifyAnalytics.lib.track != patchedTrack) {
-
-        if (typeof(window.ShopifyAnalytics.lib.track2) === 'undefined') {
-            window.ShopifyAnalytics.lib.track2 = window.ShopifyAnalytics.lib.track;
-
-            dataLayer.push({
-                'event': 'page_view_gtm',
-                'event_id': window.ShopifyAnalytics.meta.page_view_event_id,
-                'currency': window.ShopifyAnalytics.meta.currency,
-                'page_type': window.ShopifyAnalytics.meta.page ? window.ShopifyAnalytics.meta.page.pageType : '',
-                'customer_id': ShopifyAnalytics.meta.page.customerId ? ShopifyAnalytics.meta.page.customerId : '',
+OriginalImage = Image;
+Image = function () {
+    let oi = new OriginalImage();
+    oi.onload = function () {
+        function getJsonFromUrl(url) {
+            var query = url.substr(1);
+            var result = {};
+            query.split("&").forEach(function (part) {
+                var item = part.split("=");
+                result[item[0]] = decodeURIComponent(item[1]);
             });
+            return result;
         }
 
-        window.ShopifyAnalytics.lib.track = patchedTrack;
-    }
-}
+        if (this.src.indexOf('facebook.com/tr/') != -1) {
 
-patchTrack();
-setTimeout(patchTrack, 0);
-document.patchInterval = setInterval(patchTrack , 100);
-document.addEventListener("DOMContentLoaded", patchTrack);
+            var data = getJsonFromUrl(this.src);
+            var type = data.ev;
+            var eventId = data.eid;
+
+            if (type === 'PageView') {
+                dataLayer.push({
+                    'event': 'page_view_gtm',
+                    'event_id': eventId,
+                    'currency': window.ShopifyAnalytics.meta.currency,
+                    'page_type': window.ShopifyAnalytics.meta.page ? window.ShopifyAnalytics.meta.page.pageType : '',
+                    'customer_id': ShopifyAnalytics.meta.page.customerId ? ShopifyAnalytics.meta.page.customerId : '',
+                    'product': window.ShopifyAnalytics.meta.product ? window.ShopifyAnalytics.meta.product : {},
+                });
+            }
+
+            if (type === 'ViewContent' || type === 'AddToCart') {
+                dataLayer.push({
+                    'event': type === 'ViewContent' ? 'Viewed Product' : 'Added Product',
+                    'event_id': eventId,
+                    'category': data['cd[content_category]'],
+                    'product_name': data['cd[content_name]'],
+                    'product_price': parseInt(data['cd[value]']),
+                    'product_id': data['cd[content_ids]'],
+                    'currency': data['cd[currency]'],
+                    'content_type': data['cd[content_type]'],
+                    'customer_id': ShopifyAnalytics.meta.page.customerId ? ShopifyAnalytics.meta.page.customerId : '',
+                });
+            }
+        }
+    };
+    return oi;
+};
